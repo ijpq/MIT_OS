e8071833f142e7b1c5728cc3286405d09a908d1e
ijpq <509634578tk@gmail.com>
2023-11-20 19:15:36 +0800

    fix: fix pingpong code error

--- user/pingpong.c
+++ user/pingpong.c
@@ -4,23 +4,24 @@
 int main() {
   int p[2];
   pipe(p);
-  char *buf = "";
+  const int buffer_size = 512;
+  char buf[buffer_size];
 
   if (fork() == 0) { // child
-    read(p[0], buf, sizeof(buf));
+    read(p[0], buf, buffer_size);
     close(p[0]);
 
     int pid = getpid();
     fprintf(1, "%d: received ping\n", pid);
-    write(p[1], buf, sizeof(buf));
+    write(p[1], buf, buffer_size);
     close(p[1]);
     exit(0);
 
   } else { // parent
 
     char *str = "MESSAGES0";
-    write(p[1], str, sizeof(str));
-    read(p[0], buf, sizeof(buf));
+    write(p[1], str, strlen(str));
+    read(p[0], buf, buffer_size);
     close(p[0]);
     int pid = getpid();
     fprintf(1, "%d: received pong\n", pid);
